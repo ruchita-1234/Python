160b5af65d12762ae2e1d14e4734aac9c626db0b
root <root@localhost.localdomain>
2026-07-30 17:14:44 +0530

    first commit

---
+++ program.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.scanner;
+public class program
+{
+	public static void main(String arg[])
+	{
+		Scanner Sc=new Scanner(System.in);
+		system.out.println("Enter the value of a,b,c);
+		int a=Sc.nextInt();
+		int b=Sc.nextInt();
+		int c=Sc.nextInt();
+		if (a>b && a>c)
+Sytem.out.print("maximum no is"+a);
+		else
+if(b>a && b>c)
+System.out.print("maximum no is"+b);
+else
+System.out.print("maximum no is"+c);	
+
